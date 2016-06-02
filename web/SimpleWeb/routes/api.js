e153d9c77dedcf30051e88392cd71ea370d6749d
lior grossman <lior.grosman@gmail.com>
2016-06-02 13:19:10 +0300

    ok

--- web/SimpleWeb/routes/api.js
+++ web/SimpleWeb/routes/api.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var router = require('express').Router();
+var fs = require('fs');
 
 module.exports = router;
 
 router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api2!' });
+   var config = require('./users.json');
+   // var config = JSON.parse(fs.readFileSync('./routes/users.json', 'utf8'));
+   res.json({ message: 'hooray! welcome to our api25!' + config.firstName });
 });
