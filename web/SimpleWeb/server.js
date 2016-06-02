2316e1a78775d9006e87dd83eeee9527a4d40cbe
lior grossman <lior.grosman@gmail.com>
2016-06-02 10:27:25 +0300

    ח

--- web/SimpleWeb/server.js
+++ web/SimpleWeb/server.js
@@ -20,13 +20,6 @@ var port = process.env.PORT || 4532;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:4532/api)
-router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
-
-// more routes for our API will happen here
-
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', api);
